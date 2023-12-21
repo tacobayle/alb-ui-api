5b8f44f41da36f82daa9698a5a970dcacffc422b
tacobayle <nicolas.nayle@gmail.com>
2023-12-21 11:41:00 +0100

    20231221_cert-ca

--- backend/cert-ca/cert-ca.sh
+++ backend/cert-ca/cert-ca.sh
@@ -31,6 +31,8 @@ mkdir ${directory}
 echo ${ca_private_key_passphrase} | tee ${directory}/ca_private_key_passphrase.txt
 openssl genrsa -aes256 -passout pass:${ca_private_key_passphrase} -out ${directory}/${ca_name}.key ${key_size}
 openssl req -x509 -new -nodes -passin pass:${ca_private_key_passphrase} -key ${directory}/${ca_name}.key -sha256 -days ${ca_cert_days} -out ${directory}/${ca_name}.crt -subj '/CN='${CN}'/C='${C}'/ST='${ST}'/L='${L}'/O='${O}''
+sudo mkdir /var/www/html/ca-cert
+sudo cp ${directory}/${ca_name}.crt /var/www/html/ca-cert/${ca_name}.crt
 #
 #
 #
